866e80bf2eab04447f68c427cf5e1c15aba6991b
Konnor <konnor.frik666@gmail.com>
2025-09-27 19:52:33 +0500

    main:/scanner: add file for allowed litters; start implement scanner;

---
+++ scanner/liter.go
@@ -0,0 +1,23 @@
+package scanner
+
+// All allowed litters
+const (
+    LIT_A_SMALL = 'a'
+    LIT_Z_SMALL = 'z'
+    LIT_A_BIG = 'A'
+    LIT_Z_BIG = 'Z'
+
+    LIT_0 = '0'
+    LIT_9 = '9'
+
+    LIT_APOSTROPHE = '\''
+    LIT_SEMICOLON = ';'
+    LIT_PERIOD = '.'
+
+    LIT_EQUAL = '='
+    LIT_AMPERSAND = '&'
+
+    LIT_SLASH = '/'
+
+    LIT_EOL = '\n'
+)
--- scanner/scanner.go
+++ scanner/scanner.go
@@ -44,7 +44,68 @@ func (s *Scanner) ReadLexem() Lexem {
     var token = Token {
         Line: s.lineNum,
         Col: s.colNum,
+        Kind: LEX_NONE,
     }
+
+    switch {
+    case (s.char >= LIT_A_SMALL && s.char <= LIT_Z_SMALL) || (s.char >= LIT_A_BIG && s.char <= LIT_Z_BIG):
+        s.readName(&token)
+
+    case s.char >= LIT_0 && s.char <= LIT_9:
+        s.readNumber(&token)
+
+    case s.char == LIT_APOSTROPHE:
+        s.readCharConst(&token)
+
+    case s.char == LIT_SEMICOLON:
+        s.readLiter()
+        token.Kind = LEX_SEMICOLON
+
+    case s.char == LIT_PERIOD:
+        s.readLiter()
+        token.Kind = LEX_PERIOD
+
+    case s.char == litReader.EOF_CHAR:
+        token.Kind = LEX_EOF
+
+    case s.char == LIT_EQUAL:
+        s.readLiter()
+
+        if s.char == LIT_EQUAL {
+            s.readLiter()
+            token.Kind = LEX_EQL
+
+        } else {
+            token.Kind = LEX_ASSIGN
+        }
+
+    case s.char == LIT_AMPERSAND:
+        s.readLiter()
+
+        if s.char == LIT_AMPERSAND {
+            s.readLiter()
+            token.Kind = LEX_EQL
+
+        }
+
+    // TODO: implement for other symbols (or, ...)
+
+    case s.char == LIT_SLASH:
+        s.readLiter()
+
+        if s.char == LIT_SLASH {
+            for s.char != LIT_EOL && s.char != litReader.EOF_CHAR {
+                s.readLiter()
+            }
+
+            token = s.ReadLexem()
+
+        } else {
+            token.Kind = LEX_SLASH
+        }
+    }
+
+    return token
 }
 
 // read liter from input and store inside of s
@@ -56,8 +117,17 @@ func (s *Scanner) readLiter() {
         return
     }
 
-    if s.char == '\n' {
+    if s.char == LIT_EOL {
         s.colNum = 0
         s.lineNum++
     }
+}
+
+func (s *Scanner) readNumber(token *Token) {
+}
+
+func (s *Scanner) readName(token *Token) {
+}
+
+func (s *Scanner) readCharConst(token *Token) {
 }
